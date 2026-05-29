9c98bfc88727e23c3ab372598f0172ff7d2dee7c
Mahmoud-walid <modywmbadr@gmail.com>
2026-05-29 23:48:53 +0300

    Refactor icon button functions to use resolve_icon for path resolution

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -1,8 +1,28 @@
 use gtk4::prelude::*;
 use gtk4::{Button, Picture, ToggleButton};
+use std::path::PathBuf;
 
-pub fn icon_button(path: &str) -> ToggleButton {
-    let pic = Picture::for_filename(path);
+pub fn resolve_icon(name: &str) -> PathBuf {
+    // dev mode (cargo run)
+    let dev_path = PathBuf::from(format!("src/assets/{}", name));
+    if dev_path.exists() {
+        return dev_path;
+    }
+
+    // user install (AUR / local install)
+    let system_path = PathBuf::from(format!("/usr/share/wayruler/icons/{}", name));
+    if system_path.exists() {
+        return system_path;
+    }
+
+    // fallback (safe)
+    PathBuf::from(format!("/usr/share/pixmaps/{}", name))
+}
+
+// ///////////////////////////////
+
+pub fn icon_button(name: &str) -> ToggleButton {
+    let pic = Picture::for_filename(resolve_icon(name));
     pic.set_size_request(22, 22);
 
     let btn = ToggleButton::new();
@@ -11,8 +31,8 @@ pub fn icon_button(path: &str) -> ToggleButton {
     btn
 }
 
-pub fn icon_plain_button(path: &str) -> Button {
-    let pic = Picture::for_filename(path);
+pub fn icon_plain_button(name: &str) -> Button {
+    let pic = Picture::for_filename(resolve_icon(name));
     pic.set_size_request(18, 18);
 
     let btn = Button::new();
